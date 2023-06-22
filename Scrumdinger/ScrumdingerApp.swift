d2e4281bd033f5c45c60e7bd66506d105e0c9c83
Alex <56194510+rudberga@users.noreply.github.com>
2023-06-22 22:22:09 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Alexander Rudberg on 2023-06-22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
