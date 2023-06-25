86c629ab1c83600acb9e57ca94064a86e3b0445c
Alex <56194510+rudberga@users.noreply.github.com>
2023-06-25 21:11:43 +0900

    ADD - accessibility labels for cardview

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -2,16 +2,20 @@ import SwiftUI
 
 struct CardView: View {
     let scrum: DailyScrum
+    
     var body: some View {
         VStack (alignment: .leading) {
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack{
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
                     .labelStyle(.trailingIcon)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
             }
             .font(.caption)
         }
