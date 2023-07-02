36b2fb0576215ef33fb9dd8466c9e82877bfda58
Alex <56194510+rudberga@users.noreply.github.com>
2023-07-02 11:50:33 +0900

    ADD - list for and button for adding attendees

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct DetailEditView: View {
     @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form {
@@ -13,6 +14,27 @@ struct DetailEditView: View {
                     }
                 Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) {attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
                 }
             }
         }
